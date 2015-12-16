5f83f4672f788473db0e957e83f6e093d7bee19d
oihane <oihanecruce@gmail.com>
2015-12-16 15:30:33 +0100

    [FIX] ubar_custom: name_search behaviour fixed

--- ubar_custom/models/product.py
+++ ubar_custom/models/product.py
@@ -2,20 +2,25 @@
 # (c) 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, _, api
+from openerp import api, fields, models, _
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    old_reference = fields.Char('Old reference')
-    product_moves_ids = fields.One2many('product.move', 'product_id')
+    old_reference = fields.Char(string='Old reference')
+    product_moves_ids = fields.One2many(
+        comodel_name='product.move', inverse_name='product_id',
+        string='Product Moves')
 
     @api.multi
     def write(self, vals):
-        if self.old_reference != vals.get('old_reference'):
-            body = (_('The reference change from %s to %s') %
-                    (self.old_reference, vals.get('old_reference')))
+        if ('old_reference' in vals and
+                self.old_reference != vals.get('old_reference')):
+            body = (
+                _('The reference change from %s to %s') %
+                (self.old_reference or "''",
+                 vals.get('old_reference') or "''"))
             self.message_post(body=body)
         return super(ProductProduct, self).write(vals)
 
@@ -23,11 +28,13 @@ class ProductProduct(models.Model):
                     context=None, limit=100):
         if args is None:
             args = []
-        if name:
-            ids = self.search(
-                cr, user, [('old_reference', operator, name)] + args,
-                limit=limit)
-            return self.name_get(cr, user, ids, context=context)
-        return super(ProductProduct, self).name_search(
-            cr, user, name, args=args, operator=operator, context=context,
+        products = super(ProductProduct, self).name_search(
+            cr, user, name=name, args=args, operator=operator, context=context,
             limit=limit)
+        ids = [x[0] for x in products]
+        if name:
+            ids = self.search(cr, user,
+                              ['|', ('id', 'in', ids),
+                               ('old_reference', operator, name)],
+                              limit=limit, context=context)
+        return self.name_get(cr, user, ids, context=context)
--- ubar_custom/tests/test_ubar_custom.py
+++ ubar_custom/tests/test_ubar_custom.py
@@ -9,15 +9,24 @@ class TestUbarCustom(common.TransactionCase):
 
     def setUp(self):
         super(TestUbarCustom, self).setUp()
-        self.product = self.env.ref('product.product_product_6')
+        self.product = self.browse_ref('product.product_product_6')
         self.product.old_reference = 'LA001'
-        self.product_obj = self.env['product.product']
+        self.product_model = self.env['product.product']
 
     def test_name_search(self):
-        product_ids = self.product_obj.name_search(name='LA001', args=None)
+        product_ids = self.product_model.name_search(name='LA001', args=None)
         self.assertIn(self.product.id, ([a[0] for a in product_ids]),
                       'There is not any product with this old reference')
-        product_ids = self.product_obj.name_search('', args=None)
-        all_product_ids = self.product_obj.search('')
+        product_ids = self.product_model.name_search('', args=None)
+        all_product_ids = self.product_model.search('')
         self.assertEqual(len(product_ids), len(all_product_ids),
                          'Not the same quantity of products')
+
+    def test_old_reference_change(self):
+        message_count = len(self.product.message_ids)
+        self.product.write({'old_reference': 'LA001'})
+        self.assertEquals(len(self.product.message_ids), message_count,
+                          "There must not have been created a new message.")
+        self.product.write({'old_reference': 'LA002'})
+        self.assertEquals(len(self.product.message_ids), message_count + 1,
+                          "There must have been created a new message.")
