4ed7a7875be848f6a2d4c1ca3e189c4fb47fca53
esthermm <esthermartin001@gmail.com>
2016-01-15 10:52:06 +0100

    [MOD] ccg_custom: improve test

--- ccg_custom/tests/test_ccg_custom.py
+++ ccg_custom/tests/test_ccg_custom.py
@@ -16,6 +16,10 @@ class TestCCGCustom(common.TransactionCase):
             'rows': 3,
             'is_default': True,
             })
+        self.product = self.env.ref('product.product_product_18')
+        self.product.route_ids = [
+            (4, self.ref('mrp.route_warehouse0_manufacture')),
+            (4, self.ref('stock.route_warehouse0_mto'))]
 
     def test_product_packaging_default(self):
         self.mrp_production_obj = self.env['mrp.production']
@@ -26,3 +30,25 @@ class TestCCGCustom(common.TransactionCase):
         self.assertEqual(
             self.mrp_production1.product_packaging, self.product_packaging1,
             'Product packaging are not the same')
+
+    def test_mo_vals(self):
+        self.sale_order = self.env['sale.order'].create(
+            {'partner_id': self.ref('base.res_partner_2')})
+        vals = {
+            'order_id': self.sale_order.id,
+            'product_id': self.product.id,
+            'product_uom_qty': 2,
+            'product_uos_qty': 2,
+            'product_uom': self.ref('product.product_uom_unit'),
+            }
+        self.sale_order.order_line = [(0, 0, vals)]
+        self.sale_order.action_button_confirm()
+        group = self.env['procurement.group'].search([
+            ('name', '=', self.sale_order.name)])
+        procurement_order = self.env['procurement.order'].search([
+            ('group_id', '=', group.id), ('state', '=', 'confirmed')])
+        procurement_order.run()
+        mrp_production = self.env['mrp.production'].search([
+            ('id', '=', procurement_order.production_id.id)])
+        self.assertEqual(mrp_production.sale_order_id, self.sale_order)
+        self.assertEqual(mrp_production.partner_id, self.sale_order.partner_id)
