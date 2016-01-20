ff37d5f15a5d66a81abb3f8b909355e3e59654f3
oihane <oihanecruce@gmail.com>
2016-01-20 11:44:53 +0100

    [IMP] astikar_custom: do not increase sequence

--- astikar_custom/models/mrp_repair.py
+++ astikar_custom/models/mrp_repair.py
@@ -2,7 +2,7 @@
 # © 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class MrpRepair(models.Model):
@@ -11,10 +11,18 @@ class MrpRepair(models.Model):
     def _defaul_quotation_notes(self):
         return self.env.user.company_id.sale_note
 
-    def create(self, vals):
-        vals['name'] = self.env['ir.sequence'].get('mrp.repair') or '/'
-        new_id = super(MrpRepair, self).create(vals)
-        return new_id
-
-    name = fields.Char(default=False, required=False)
+    name = fields.Char(default='/')
     quotation_notes = fields.Text(default=_defaul_quotation_notes)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', '/') == '/':
+            vals['name'] = self.env['ir.sequence'].get('mrp.repair')
+        return super(MrpRepair, self).create(vals)
+
+    @api.multi
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        default['name'] = self.env['ir.sequence'].get('mrp.repair')
+        return super(MrpRepair, self).copy(default)
--- astikar_custom/tests/test_astikar_custom.py
+++ astikar_custom/tests/test_astikar_custom.py
@@ -9,7 +9,9 @@ class TestAstikarCustom(common.TransactionCase):
 
     def setUp(self):
         super(TestAstikarCustom, self).setUp()
+        self.ir_sequence_model = self.env['ir.sequence']
         self.mrp_repair_model = self.env['mrp.repair']
+        self.mrp_repair_sequence = self.ref('mrp_repair.seq_mrp_repair')
 
     def test_default_quotation_note(self):
         note = 'Test Sale Note'
@@ -21,3 +23,31 @@ class TestAstikarCustom(common.TransactionCase):
                          self.env.user.company_id.sale_note)
         self.assertEqual(repair.quotation_notes,
                          note)
+
+    def test_new_repair_sequence_assign(self):
+        name = self._get_next_name()
+        mrp_repair = self.mrp_repair_model.create({
+            'name': 'Testing MRP repair name',
+        })
+        self.assertNotEqual(mrp_repair.name, '/')
+        self.assertEqual(mrp_repair.name, name)
+
+    def test_copy_repair_sequence_assign(self):
+        name = self._get_next_name()
+        mrp_repair = self.mrp_repair_model.create({
+            'name': 'Testing MRP repair name',
+        })
+        mrp_repair_copy = mrp_repair.copy()
+        self.assertNotEqual(mrp_repair_copy.name, mrp_repair.name)
+        self.assertEqual(mrp_repair_copy.name, name)
+
+    def _get_next_name(self):
+        d = self.ir_sequence_model._interpolation_dict()
+        prefix = self.ir_sequence_model._interpolate(
+            self.mrp_repair_sequence.prefix, d)
+        suffix = self.ir_sequence_model._interpolate(
+            self.mrp_repair_sequence.suffix, d)
+        name = (prefix + ('%%0%sd' % self.mrp_repair_sequence.padding %
+                          self.mrp_repair_sequence.number_next_actual) +
+                suffix)
+        return name
