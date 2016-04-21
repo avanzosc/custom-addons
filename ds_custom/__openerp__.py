edc338d6517224582f84c12cb7c8c97f325307a5
agaldona <ainaragaldona@avanzosc.es>
2016-04-21 08:40:46 +0200

    [ADD] ds_custom: modulo especifico para cliente

---
+++ ds_custom/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "DS - Custom",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+    ],
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/ds_custom_css.xml"
+    ],
+    "installable": True,
+}
