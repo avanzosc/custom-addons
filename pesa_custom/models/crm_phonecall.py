09bed2d5f8676dbcd2a44d15c0989c208a52df49
Esther Martín <esthermartin001@gmail.com>
2016-05-09 12:30:47 +0200

    [MOD] pesa_custom: test added, travis changes

--- pesa_custom/models/crm_phonecall.py
+++ pesa_custom/models/crm_phonecall.py
@@ -2,7 +2,7 @@
 # (c) 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import _, api, fields, models
+from openerp import _, api, models
 
 
 class CrmPhonecall(models.Model):
--- pesa_custom/wizard/make_call.py
+++ pesa_custom/wizard/make_call.py
@@ -2,7 +2,7 @@
 # © 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import _, api, models, fields
+from openerp import _, api, models
 
 
 class MakeCall(models.TransientModel):
