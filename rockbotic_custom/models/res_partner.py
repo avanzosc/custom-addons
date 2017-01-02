9217952a75ee5c6043d2c3dbd43a2a3aa0652a14
oihane <oihanecruce@gmail.com>
2017-01-02 17:05:14 +0100

    [IMP] rockbotic_custom: student and teachers in other sheet

--- rockbotic_custom/models/res_partner.py
+++ rockbotic_custom/models/res_partner.py
@@ -11,3 +11,17 @@ class ResPartner(models.Model):
     web_blog = fields.Boolean(string='Web/Blog', default=False)
     social_networks = fields.Boolean(string='Social networks', default=False)
     student_rockbotic_level = fields.Char(string='Rockbotic level')
+    student_child_ids = fields.One2many(
+        comodel_name='res.partner', inverse_name='parent_id',
+        string='Students',
+        domain=[('active', '=', True), ('registered_partner', '=', True),
+                ('employee_id', '=', False)])
+    teacher_child_ids = fields.One2many(
+        comodel_name='res.partner', inverse_name='parent_id',
+        string='Teachers',
+        domain=[('active', '=', True), ('registered_partner', '=', True),
+                ('employee_id', '!=', False)])
+    other_child_ids = fields.One2many(
+        comodel_name='res.partner', inverse_name='parent_id',
+        string='Contacts',
+        domain=[('active', '=', True), ('registered_partner', '=', False)])
