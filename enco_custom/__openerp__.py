82e6d8cf355e81095f47ad2c85d2f9245264ac3d
Esther Martín <esthermartin001@gmail.com>
2017-01-18 09:19:40 +0100

    [ADD] enco_custom: new module

---
+++ enco_custom/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Enco - Custom",
+    "version": "8.0.1.0.0",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "category": "Custom module",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Esther Martín <esthermartin@avanzosc.es>",
+    ],
+    "depends": [
+        "sale_stock",
+        ],
+    "data": [
+        "report/report_layout.xml",
+        "report/report_paperformat.xml",
+        "views/sale_order_view.xml",
+        ],
+    "installable": True
+}
