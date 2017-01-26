8f6f5ea4ee83cbc25144e578961fcda1c057292c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-01-26 12:29:27 +0100

    [IMP] galaxia_custom: In sale order amounts with 5 decimals, but show only 2 decimals.

--- galaxia_custom/models/sale_order.py
+++ galaxia_custom/models/sale_order.py
@@ -2,10 +2,18 @@
 # © 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    amount_untaxed = fields.Float(
+        string='Untaxed Amount', digits=dp.get_precision('Precision in sales'))
+    amount_tax = fields.Float(
+        string='Total', digits=dp.get_precision('Precision in sales'))
+    amount_total = fields.Float(
+        string='Total', digits=dp.get_precision('Precision in sales'))
 
     @api.multi
     def action_button_confirm(self):
@@ -25,3 +33,7 @@ class SaleOrderLine(models.Model):
                 line.product_type = line.product_tmpl_id.type
 
     apply_performance = fields.Boolean(default=False)
+    price_unit = fields.Float(
+        string='Unit Price', digits=dp.get_precision('Precision in sales'))
+    price_subtotal = fields.Float(
+        string='Subtotal', digits=dp.get_precision('Precision in sales'))
