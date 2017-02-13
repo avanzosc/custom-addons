66a5578761b607d04cdf0fc15ca7e280726dce30
Esther Martín <esthermartin001@gmail.com>
2017-02-13 10:35:34 +0100

    [ADD] new module be_custom

---
+++ be_custom/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "BE - Custom",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+    ],
+    "depends": [
+        "report",
+    ],
+    "data": [
+        "report/layout_view.xml",
+    ],
+    "installable": True,
+}
