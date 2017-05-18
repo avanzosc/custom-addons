9511707b8eb8d873f3e05d0a7f15f21b41f96c0d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-05-18 15:51:50 +0200

    [IMP] galaxia_custom: Customize partners to show in presences.

--- galaxia_custom/tests/test_galaxia_custom.py
+++ galaxia_custom/tests/test_galaxia_custom.py
@@ -8,9 +8,12 @@ class TestGalaxiaCustom(common.TransactionCase):
 
     def setUp(self):
         super(TestGalaxiaCustom, self).setUp()
+        self.registration_model = self.env['event.registration']
         self.account_model = self.env['account.analytic.account']
         self.project_model = self.env['project.project']
         self.sale_model = self.env['sale.order']
+        self.event_model = self.env['event.event']
+        self.wiz_add_model = self.env['wiz.event.append.assistant']
         account_vals = {'name': 'account procurement service project',
                         'date_start': '2016-01-15',
                         'date': '2016-02-28'}
@@ -50,10 +53,38 @@ class TestGalaxiaCustom(common.TransactionCase):
             'thursday': True}
         sale_vals['order_line'] = [(0, 0, sale_line_vals)]
         self.sale_order = self.sale_model.create(sale_vals)
+        self.partner = self.env['res.partner'].create({
+            'name': 'Test Partner',
+        })
+        self.env['marketing.config.settings'].create(
+            {'module_marketing_campaign': False,
+             'module_mass_mailing': False,
+             'module_crm_profiling': False,
+             'module_marketing_campaign_crm_demo': False,
+             'show_all_customers_in_presences': True})
+        self.env['ir.config_parameter'].create({
+            'key': 'show.all.customers.in.presences',
+            'value': True})
 
     def test_galaxia_custom(self):
         self.sale_order.action_button_confirm()
         self.sale_order.order_line[0]._compute_product_type()
-        self.assertNotEqual(
-            len(self.sale_order.order_line), 0,
-            'Sale order withour lines')
+        self.assertNotEqual(len(self.sale_order.order_line), 0,
+                            'Sale order withour lines')
+        cond = [('sale_order', '=', self.sale_order.id)]
+        event = self.event_model.search(cond, limit=1)[:1]
+        registration_vals = {
+            'event_id': event.id,
+            'partner_id': self.partner.id,
+            'date_start': '2016-01-15 15:00:00',
+            'date_end': '2016-02-28 18:00:00'}
+        registration = self.registration_model.create(registration_vals)
+        dict_add_wiz = registration.button_registration_open()
+        add_wiz = self.wiz_add_model.with_context(
+            active_ids=event.ids).browse(dict_add_wiz.get('res_id'))
+        add_wiz.action_append()
+        session = event.track_ids[0]
+        session.presences[0]._compute_allowed_partner_ids()
+        self.assertEqual(len(session.allowed_partner_ids),
+                         len(session.presences[0].allowed_partner_ids),
+                         'Bad allowed partners')
