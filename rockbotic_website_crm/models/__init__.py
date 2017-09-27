81e19ff1b8da4b343a27d4e528062e605f0e9b2a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-09-27 15:55:31 +0200

    [IMP] rockbotic_website_crm: Send email when create and inscription.

--- rockbotic_website_crm/models/__init__.py
+++ rockbotic_website_crm/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2017 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from . import crm_lead
+from . import event_registration
+from . import res_company
