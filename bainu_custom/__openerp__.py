3817dacb55d2bfd31b9587e329fe3a2eaaea8617
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-10-10 15:55:02 +0200

    [ADD] bainu_custom: New field "Almu" in invoices, and Customized invoices report.

---
+++ bainu_custom/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Bainu - Custom",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "depends": [
+        "account_voucher",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+        "report/invoice_reports.xml",
+    ],
+    "installable": True,
+}
