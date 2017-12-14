5f64a2df9c5b46cc83d7583b8f9893923a442208
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-12-14 11:58:21 +0100

    [IMP] galaxia_custom: Scheduled time control when confirm one sale order.

--- galaxia_custom/models/sale_order.py
+++ galaxia_custom/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions, _
 import openerp.addons.decimal_precision as dp
 
 
@@ -50,6 +50,11 @@ class SaleOrder(models.Model):
     @api.multi
     def action_button_confirm(self):
         event_model = self.env['event.event']
+        if (any(self.mapped('order_line.product_id.recurring_service')) and
+                self.project_id and not self.project_id.working_hours):
+            raise exceptions.Warning(
+                _("Yout must define one Working Schedule, in sale contract: "
+                  "%s") % self.project_id.name)
         result = super(SaleOrder, self.with_context(
             without_sale_name=True)).action_button_confirm()
         cond = [('sale_order', '=', self.id)]
