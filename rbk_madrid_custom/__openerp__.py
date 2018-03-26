e6fc95c65f8b2744bf93a3243824f7acdc0869f1
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-03-26 10:19:16 +0200

    [ADD] rbk_madrid_custom: Fields related from employees to partners: Work email, and work phone.

---
+++ rbk_madrid_custom/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2018 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Rbk Madrid Custom",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "category": "Human Resources",
+    "depends": [
+        "hr_employee_catch_partner",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
