217b3e649df445d4320d066068fcccf3ead025e8
oihane <oihanecruce@gmail.com>
2018-07-23 14:37:12 +0200

    [IMP] ccg_custom: fix account_invoice_kanban for gemma (#354)

--- ccg_custom/__openerp__.py
+++ ccg_custom/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Esther Martín - AvanzOSC
+# Copyright 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "CCG Custom",
-    "version": "8.0.2.0.0",
+    "version": "8.0.3.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
@@ -22,6 +22,7 @@
         "product_supplierinfo_for_customer",
         "sale_order_dates",
         "sale_stock",
+        "account_invoice_kanban",
     ],
     "category": "Custom Module",
     "data": [
@@ -34,5 +35,5 @@
         "views/product_view.xml",
         "views/stock_picking_view.xml"
     ],
-    "installable": True
+    "installable": True,
 }
