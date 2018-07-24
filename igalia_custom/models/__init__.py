a8b6ab3292cdba02228238115821127a81a0a454
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-07-24 14:42:33 +0200

    [IMP] igalia_custom: Calculate depreciation percentage on assets.

--- igalia_custom/models/__init__.py
+++ igalia_custom/models/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # © 2015 Esther Martín - AvanzOSC
+# Copyright 2017 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import product_template
 from . import account_invoice
 from . import account_analytic_line
+from . import account_asset_asset
