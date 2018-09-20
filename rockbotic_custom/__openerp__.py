0b9554b0313f5f31df6498e60eb86dada5cdf5e8
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-09-20 11:01:25 +0200

    [IMP] rockbotic_custom: añadir campos para clasificar los eventos

--- rockbotic_custom/__openerp__.py
+++ rockbotic_custom/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Rockbotic - Custom",
-    "version": "8.0.2.5.0",
+    "version": "8.0.2.6.0",
     "category": "Custom Module",
     "license": "AGPL-3",
     "author": "AvanzOSC",
