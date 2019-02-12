4cae052d01658e2abe14c51aa5fa9f76be30d733
Daniel-CA <danielcampos@avanzosc.es>
2019-02-12 11:48:40 +0100

    [IMP] tumaker_custom: Relaxed group security for date_done on stock picking view

--- tumaker_custom/__openerp__.py
+++ tumaker_custom/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Tumaker custom",
-    "version": "8.0.2.1.0",
+    "version": "8.0.2.2.0",
     "license": "AGPL-3",
     "depends": ["sale_order_dates",
                 "project_timesheet",
