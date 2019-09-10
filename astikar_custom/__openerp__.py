3200f26cc9adec4cc1b56a32c93d1cd015ae5949
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2019-09-10 09:28:00 +0200

    [IMP] astikar_custom: En impresión facturas, poner en negrita las notas de la forma de pago.

--- astikar_custom/__openerp__.py
+++ astikar_custom/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Astikar - Custom",
-    "version": "8.0.3.14.0",
+    "version": "8.0.3.14.1",
     "category": "Custom Module",
     "license": "AGPL-3",
     "author": "AvanzOSC",
