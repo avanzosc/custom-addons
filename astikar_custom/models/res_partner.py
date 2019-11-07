44ecc418e59c4afded94f87ad34947c3717f1aa3
Daniel-CA <danielcampos@avanzosc.es>
2019-11-07 11:34:27 +0100

    [IMP] astikar_custom: Warning when adding new partner with existing VAT

--- astikar_custom/models/res_partner.py
+++ astikar_custom/models/res_partner.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Esther Martín - AvanzOSC
+# Copyright (c) 2016 Esther Martín - AvanzOSC
+# Copyright (c) 2019 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from openerp import api, models, fields, _
 
 
 class ResPartner(models.Model):
@@ -14,3 +15,18 @@ class ResPartner(models.Model):
         comodel_name='crm.tracking.medium', string="Channel")
     source_id = fields.Many2one(comodel_name='crm.tracking.source',
                                 string='Source')
+
+    @api.multi
+    @api.onchange('vat')
+    def vat_change(self, value):
+        res = super(ResPartner, self).vat_change(value=value)
+        if value:
+            result = self.search([('vat', '=', value), ])
+            if result:
+                res.update({
+                    'warning': {
+                        'title': _('Info'),
+                        'message': _('VAT already exist: {}').format((value)),
+                        },
+                })
+        return res
