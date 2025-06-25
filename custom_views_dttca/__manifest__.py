7a2653152fb3a5eca27009ef46f6a2d9fcbb19b9
Alfredo <alfredodelafuente@avanzosc.es>
2025-06-25 11:37:26 +0200

    [16.0][ADD] custom_views_dttca: Personalized sale order tree view. Decoration depend of "delivery_status".

---
+++ custom_views_dttca/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Dietetica Custom Views",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "views/sale_order_views.xml"
+    ],
+    "installable": True,
+}
