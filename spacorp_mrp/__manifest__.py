e44f06a2992c1df7d24a985451b1412d6da3bfeb
Alfredo <alfredodelafuente@avanzosc.es>
2025-08-20 13:14:43 +0200

    [16.0][ADD] spacorp_mrp: New group "Show product and BoM cost in printed report".

---
+++ spacorp_mrp/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Manufacturing - SPA Corp customization",
+    "version": "18.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "excludes": [],
+    "data": [
+        "security/spacorp_mrp_groups.xml",
+        "report/mrp_report_bom_structure.xml",
+    ],
+    "installable": True,
+}
