8dbec37a7bd97edacbb03b9a9efe334e96333557
Alfredo <alfredodelafuente@avanzosc.es>
2025-12-23 07:59:23 +0100

    [18.0][ADD] mpf_reports: Mpf customized layout.

---
+++ mpf_reports/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mpf Reports",
+    "version": "18.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "report/report_layouts.xml",
+    ],
+    "installable": True,
+}
