cc55174d4349da892189fc3f67f724abf6cc3a0f
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-08 13:21:22 +0100

    [18.0][DEL] mpf_sale_account: Replaced by mpf_crm.

--- mpf_sale_account/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import models
-
-
-def _post_install_put_sale_lead_in_account_invoice(env):
-    cond = [("opportunity_id", "!=", False)]
-    sales = env["sale.order"].search(cond)
-    for sale in sales:
-        if sale.invoice_ids:
-            sale.invoice_ids.write({"opportunity_id": sale.opportunity_id.id})
