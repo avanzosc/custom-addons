ddd397a6ea7f70447b545fe3ba1a63ea5fbd1a4d
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-21 08:03:01 +0100

    [18.0][ADD] oktn_custom_invoice_views: Customized account invoice tree view.

---
+++ oktn_custom_invoice_views/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Alejandro Nieto - Okatent
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Oktn Custom Invoice Views",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "depends": [
+        "account",
+        "l10n_es_aeat_sii_oca",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+}
