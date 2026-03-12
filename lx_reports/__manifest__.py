73207398ec370986badb1c27e023063e315f178b
Alfredo <alfredodelafuente@avanzosc.es>
2026-03-12 14:11:47 +0100

    [18.0][ADD] lx_reports: Customized reports.

---
+++ lx_reports/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Lx Reports",
+    "version": "18.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "depends": ["web", "stock", "delivery", "stock_delivery"],
+    "data": [
+        "reports/external_layout_standard_templates.xml",
+        "reports/report_delivery_document_inherit.xml",
+    ],
+}
