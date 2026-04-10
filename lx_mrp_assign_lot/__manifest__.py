9cee54591c924ac1b6fb0d04f1008f6181e912db
Alfredo <alfredodelafuente@avanzosc.es>
2026-04-10 08:37:50 +0200

    [18.0][ADD] lx_mrp_assign_lot: When the batch is generated for the product to be produced, the format is "3601"+"OF name".

---
+++ lx_mrp_assign_lot/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Lx Mrp Assign Lot",
+    "version": "18.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "depends": ["stock", "mrp"],
+    "data": [],
+}
