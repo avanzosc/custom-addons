098a26c0afa140d933dd65be35e4cc4140a87b18
Alfredo <alfredodelafuente@avanzosc.es>
2026-07-21 13:05:26 +0200

    [18.0][ADD] oktn_custom_invoice_report: New reports for invoices.

---
+++ oktn_custom_invoice_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Oktn Custom Invoice Report",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Accounting'",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/custom-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "reports/account_invoice_plegable_report.xml",
+        "reports/account_invoice_plegable_with_payments_report.xml",
+    ],
+    "installable": True,
+}
